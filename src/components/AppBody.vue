06f91df2c5bfd83c8392bd19366ad4db970712bf
Loren Rachelle Hicks <lorenhicks@gmail.com>
2022-02-01 19:33:08 -0500

    VueX Store created + Unit test passing state

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -13,9 +13,12 @@
 </template>
 <script>
 import { computed } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
   setup () {
+    const store = useStore()
+    console.log('###CONSOLE LOGGER', store.state.count)
     const volunteer = computed(() => [
       { name: 'Davido', pronouns: 'he, him', id: '777', county: 'Orange', active: 'active' },
       { name: 'Loren', pronouns: 'she, hers', id: '333', county: 'Sarasota', active: 'active' },
