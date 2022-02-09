a9f3d5aa6f63184549af377328510d19e14af674
Loren Rachelle Hicks <lorenhicks@gmail.com>
2022-02-09 17:04:54 -0500

    Initializing plugins

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+/* eslint-disable */
+// @ts-ignore
+import { FontAwesomeIcon } from './plugins/font-awesome'
+/* eslint-enable */
 
-const app = createApp(App).use(store).use(router)
+const app = createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon)
 app.config.globalProperties.$log = console.log
 app.mount('#app')
