5383de861c92aa6574b6291aa0944018ada3d53b
Loren Rachelle Hicks <lorenhicks@gmail.com>
2022-02-11 18:24:25 -0500

    Initializing mongoose

--- node-js-jwt-auth-mongodb/server.js
+++ node-js-jwt-auth-mongodb/server.js
@@ -10,6 +10,21 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 // simple route
+const db = require("./app/models");
+const Role = db.role;
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to myVolunteer server." });
 });
@@ -18,3 +33,33 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`myVolunteer server is running on port ${PORT}.`);
 });
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'moderator' to roles collection");
+      });
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
