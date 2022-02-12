17adf24ab73a62a0f0dec62324abb3c02b151409
Loren Rachelle Hicks <lorenhicks@gmail.com>
2022-02-11 20:17:33 -0500

    Routes defined for authentication & authorization

--- node-js-jwt-auth-mongodb/controllers/auth.controller.js
+++ node-js-jwt-auth-mongodb/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const config = require("../config/auth.config");
+const config = require("../app/config/auth.config");
 const db = require("../models");
 const User = db.user;
 const Role = db.role;
