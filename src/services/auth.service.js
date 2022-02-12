50faa9c8d447b83fffae7f25de0bdff808a4b013
Loren Rachelle Hicks <lorenhicks@gmail.com>
2022-02-12 14:42:13 -0500

    Reconfiguring API url paths

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-const API_URL = 'http://localhost:8080/'
+import axios from 'axios';
+const API_URL = 'http://localhost:8080/api/auth/';
 class AuthService {
-  login (user) {
+  login(user) {
     return axios
       .post(API_URL + 'signin', {
         username: user.username,
@@ -9,22 +9,20 @@ class AuthService {
       })
       .then(response => {
         if (response.data.accessToken) {
-          localStorage.setItem('user', JSON.stringify(response.data))
+          localStorage.setItem('user', JSON.stringify(response.data));
         }
-        return response.data
-      })
+        return response.data;
+      });
   }
-
-  logout () {
-    localStorage.removeItem('user')
+  logout() {
+    localStorage.removeItem('user');
   }
-
-  register (user) {
+  register(user) {
     return axios.post(API_URL + 'signup', {
       username: user.username,
       email: user.email,
       password: user.password
-    })
+    });
   }
 }
-export default new AuthService()
+export default new AuthService();
