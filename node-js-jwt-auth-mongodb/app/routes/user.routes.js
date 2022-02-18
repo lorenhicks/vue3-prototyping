1163c209211da262893ec985e5104bb78a991a38
Loren Rachelle Hicks <lorenhicks@gmail.com>
2022-02-18 00:42:31 -0500

    Debugging token verification

--- node-js-jwt-auth-mongodb/app/routes/user.routes.js
+++ node-js-jwt-auth-mongodb/app/routes/user.routes.js
@@ -9,15 +9,13 @@ module.exports = function(app) {
     next();
   });
   app.get("/api/test/all", controller.allAccess);
-  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+  app.get("/api/test/user", controller.userBoard);
   app.get(
     "/api/test/mod",
-    [authJwt.verifyToken, authJwt.isModerator],
     controller.moderatorBoard
   );
   app.get(
     "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
     controller.adminBoard
   );
 };
--- src/components/BoardUser.vue
+++ src/components/BoardUser.vue
@@ -1,53 +1,21 @@
 <template>
-  <div>
-      <h2>{{ currentUser.username }}'s Volunteer Log</h2>
-      <hr />
-      <div class="alert alert-success" v-if="isSuccess">
-          Post Created Successfully
-      </div>
-      <form @submit.prevent="onCreatePost">
-          <div class="form-group">
-              <label>County</label>
-              <input type="text" class="form-control" v-model="county" />
-          </div>
-          <div class="form-group">
-              <label>Activities</label>
-              <textarea
-                  class="form-control"
-                  v-model="activities"
-              ></textarea>
-          </div>
-          <div class="mt-3">
-              <button type="submit" class="btn btn-primary">
-                  Add Entry
-              </button>
-              &nbsp;
-              <button type="reset" class="btn btn-secondary">
-                  Clear Fields
-              </button>
-          </div>
-      </form>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>{{ content }}</h3>
+    </header>
   </div>
 </template>
 <script>
-import axios from 'axios'
 import UserService from '../services/user.service'
 export default {
   name: 'User',
   data () {
     return {
-      county: '',
-      activities: '',
-      isSuccess: false
-    }
-  },
-  computed: {
-    currentUser () {
-      return this.$store.state.auth.user
+      content: ''
     }
   },
   mounted () {
-    UserService.getPublicContent().then(
+    UserService.getUserBoard().then(
       (response) => {
         this.content = response.data
       },
@@ -60,19 +28,6 @@ export default {
           error.toString()
       }
     )
-  },
-  methods: {
-    onCreatePost () {
-      axios.post('http://localhost:8080/post.json', {
-        county: this.county,
-        activities: this.activities
-      })
-        .then((response) => {
-          this.isSuccess = true
-          console.log('Post test', response)
-          this.$emit('postcreated')
-        })
-    }
   }
 }
 </script>
